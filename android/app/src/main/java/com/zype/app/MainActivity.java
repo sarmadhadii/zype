1106b77018b24e8bca32ffd637e0133b77aa7405
sarmadhadii <sarmadhaddi@gmail.com>
2023-03-31 06:08:15 +0500

    Cap files

---
+++ android/app/src/main/java/com/zype/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.zype.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zype.app',
+  appName: 'Zype',
+  webDir: 'dist/zype-app',
+  bundledWebRuntime: false
+};
+
+export default config;
